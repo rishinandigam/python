caf09ee3976732763e56f3ef9a069cc2e247dcdc
rishinandigam <gnyanarushi@gmail.com>
2024-06-05 11:06:42 +0530

    python prgrms

---
+++ python/condition.py
@@ -0,0 +1,8 @@
+# name = "rishiKumar"
+# print(name)
+# x = 2001
+# print("the date of birth is : ",x)
+# if x < 2005 :
+#     print("He is eligible to vote.")
+# else :
+#     print("not eligible to vote")
---
+++ python/loops.py
@@ -0,0 +1,8 @@
+# for x in range (1,100):
+#     print(x, "hello python") 
+i=0 
+while i<9:
+    {
+        print("I love you")
+        i +=1 
+    }
